0c8d5d8de5952f706a252697c8cf4a21a16eaf12
NiroshaDilan <dilan.vista@gmail.com>
2020-01-23 23:53:59 +0530

    initial project creation

---
+++ src/main/java/com/icoder/springbootweblogic/SpringbootWeblogicApplication.java
@@ -0,0 +1,13 @@
+package com.icoder.springbootweblogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWeblogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWeblogicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icoder/springbootweblogic/SpringbootWeblogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icoder.springbootweblogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWeblogicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
